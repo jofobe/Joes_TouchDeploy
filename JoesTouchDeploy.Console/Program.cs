a76b677cbfb9e1941a79d84b4c30f1666c32fabe
jofobe <jody.benjamin@colorado.edu>
2026-06-26 15:08:04 -0600

    Sprint 1.2 - Add networking layer and panel connectivity

--- JoesTouchDeploy.Console/Program.cs
+++ JoesTouchDeploy.Console/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JoesTouchDeploy.Core.Networking;
+
+Console.WriteLine("=================================");
+Console.WriteLine("Joe's TouchDeploy");
+Console.WriteLine("=================================");
+Console.WriteLine();
+
+var client = new CrestronHttpClient();
+
+Console.Write("Panel IP: ");
+
+var ip = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("Connecting...");
+Console.WriteLine();
+
+try
+{
+    var html = await client.GetLoginPageAsync(ip!);
+
+    Console.WriteLine("Connection successful!");
+    Console.WriteLine();
+    Console.WriteLine($"Downloaded {html.Length:N0} characters.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Connection failed: {ex.Message}");
+}
